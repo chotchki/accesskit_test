69c9a08ed60c8c1743f6c867470894a7e129599e
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2023-05-20 11:48:43 -0400

    Added code to reproduce

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,72 @@
+use bevy::prelude::*;
+use bevy::window::close_on_esc;
+use bevy::winit::{WinitSettings, UpdateMode};
+use bevy::diagnostic::{FrameTimeDiagnosticsPlugin, LogDiagnosticsPlugin};
+use bevy::utils::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+    .add_plugins(DefaultPlugins)
+    .add_plugin(FrameTimeDiagnosticsPlugin::default())
+    .add_plugin(LogDiagnosticsPlugin::default())
+    .insert_resource(
+        WinitSettings {
+            focused_mode: UpdateMode::Reactive {max_wait: Duration::MAX},
+            unfocused_mode: UpdateMode::Reactive {max_wait: Duration::MAX},
+            return_from_run: false,
+        }
+    )
+    .add_startup_system(setup)
+    .add_system(close_on_esc)
+    .run();
+}
+
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    commands.spawn(Camera2dBundle::default());
+    commands.spawn(
+        NodeBundle {
+            style: Style {
+                size: Size::width(Val::Percent(100.0)),
+                ..default()
+            },
+            background_color: Color::RED.into(),
+            ..default()
+        }
+    )
+    .with_children(|parent| {
+        parent.spawn((
+            TextBundle::from_section(
+                "Hello world!",
+                TextStyle {
+                    font: assets.load("FSEX300.ttf"),
+                    font_size: 100.0,
+                    color: Color::WHITE,
+                }
+            ),
+            Label,
+        ));
+    });
+    commands.spawn(
+        NodeBundle {
+            style: Style {
+                size: Size::width(Val::Percent(100.0)),
+                ..default()
+            },
+            background_color: Color::RED.into(),
+            ..default()
+        }
+    )
+    .with_children(|parent| {
+        parent.spawn((
+            TextBundle::from_section(
+                "Hello world!",
+                TextStyle {
+                    font: assets.load("FSEX300.ttf"),
+                    font_size: 100.0,
+                    color: Color::WHITE,
+                }
+            ),
+            Label,
+        ));
+    });
 }
